96e5b8f23f10492927b5af96b71f4567e8bd0782
NanoDesu! <20625307+wuchenkang@users.noreply.github.com>
2020-09-07 19:35:14 +0800

    Update sysuh3c.lua

--- luci-app-sysuh3c/luasrc/model/cbi/sysuh3c.lua
+++ luci-app-sysuh3c/luasrc/model/cbi/sysuh3c.lua
@@ -22,7 +22,7 @@ s.anonymous = true
 enable = s:option(Flag, "enable", translate("Enable"))
 blockstartup = s:option(Flag, "blockstartup", translate("Block Startup Sequence"),
 	translate("If enabled, the client will block startup sequence until authenticated"))
-name = s:option(Value, "username", translate("Username"))
+name = s:option(Value, "username", translate("NetID"))
 pass = s:option(Value, "password", translate("Password"))
 pass.password = true
 
